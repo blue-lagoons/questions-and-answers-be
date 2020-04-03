71784a53c2a128a5c0f8f3dd320b461e7a5882b6
Shawn Sullivan <sullivansm3@gmail.com>
2020-04-03 15:09:43 -0600

    Updated outlay

--- nosql-db/schema-example.js
+++ nosql-db/schema-example.js
@@ -1,33 +1,37 @@
 {
-    "_id": 1,
-    "product_id": 1,
-    "body": "Does the fit well?",
-    "date": "2018-01-04",
-    "asker_name": "yankeelover",
-    "asker_email": "first.last@gmail.com",
-    "reported": 0,
-    "helpful": 1,
-    "answers": [
-        {
-            "answer_id": 148,
-            "body": "Voluptatem in qui quasi.",
-            "date_written": "2019-06-07",
-            "answerer_name": "Colt.Bergnaum49",
-            "answerer_email": 'first.last@gmail.com',
-            "helpfulness": 10,
-            "photos": [
-                "https://images.unsplash.com/photo-1428790067070-0ebf4418d9d8?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
-            ]
-        },
-        ...
-        {
-            "answer_id": 149,
-            "body": "Aut architecto dolores quisquam in impedit facilis.",
-            "date_written": "2019-07-15",
-            "answerer_name": "Rosina19",
-            "answerer_email": 'first.last@gmail.com',
-            "helpfulness": 491,
-            "photos": ['url1', 'url2', ..., 'url-nth']
-        },
-    ]
+    {
+        "_id": 1,
+        "product_id": 1,
+        "body": "Does the fit well?",
+        "date": "2018-01-04",
+        "asker_name": "yankeelover",
+        "asker_email": "first.last@gmail.com",
+        "reported": 0,
+        "helpful": 1,
+        "answers": [
+            {
+                "answer_id": 148,
+                "body": "Voluptatem in qui quasi.",
+                "date_written": "2019-06-07",
+                "answerer_name": "Colt.Bergnaum49",
+                "answerer_email": 'first.last@gmail.com',
+                "helpfulness": 10,
+                "photos": [
+                    "https://images.unsplash.com/photo-1428790067070-0ebf4418d9d8?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
+                ]
+            },
+            ...
+            {
+                "answer_id": 149,
+                "body": "Aut architecto dolores quisquam in impedit facilis.",
+                "date_written": "2019-07-15",
+                "answerer_name": "Rosina19",
+                "answerer_email": 'first.last@gmail.com',
+                "helpfulness": 491,
+                "photos": ['url1', 'url2', ..., 'url-nth']
+            },
+        ]
+    },
+    {
+    }
 }
