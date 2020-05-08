c338e653968414462017e6186c011860f087e381
Shawn Sullivan <sullivansm3@gmail.com>
2020-05-08 09:31:58 -0600

    Minor code clean up

--- script.js
+++ script.js
@@ -4,12 +4,11 @@ import { Rate } from "k6/metrics";
 export let errorRate = new Rate("errors");
 
 export let options = {
-    vus: 60,            // A number specifying the number of VUs to run concurrently
-    duration: '5m',    // m for min
+    vus: 115,            // A number specifying the number of VUs to run concurrently
+    duration: '30s',    // m for min, s for seconds
     // rps: 100,           // The maximum number of requests to make per second, in total across all VUs
     throw: true,         // throw errors on failed HTTP requests
-    // discardResponseBodies: true, //discard repsponse bodies to lessen the amount of mem required
-    thresholds: {http_req_duration: ["max<2000"]},
+    thresholds: {http_req_duration: ["max<2000"]}, // setting a test requirement
 };
 
 export default function() {
@@ -21,7 +20,7 @@ export default function() {
       check(res, {
         'status was 200': r => r.status == 200,
       }) || errorRate.add(1);
-      sleep(.001);
+      sleep(.004); //in seconds (therefore 0.001 is 1 ms)
   }
 
   // for (let i = 0; i < 100; i++) {
